260f9192f747a2ed7c860a6768f79552dc7b4ba1
Hachibitz <felipealmeida.ns@outlook.com>
2022-11-25 11:01:28 -0300

    Producer - removendo import indevido

--- kafka-producer/src/main/java/com/learning/kafka/producer/controller/CarController.java
+++ kafka-producer/src/main/java/com/learning/kafka/producer/controller/CarController.java
@@ -1,9 +1,6 @@
 package com.learning.kafka.producer.controller;
 
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
